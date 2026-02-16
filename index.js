af8ea7100dd7e02cf669302d8a82ff2088b41f6c
Harpreet Singh <happu20022@gmail.com>
2026-02-16 14:12:25 +0530

    added connectDB() in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config.js';
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 import resumeRoutes from './src/routes/resumeRoutes.js';
+import { connectDB } from './src/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +15,8 @@ app.use((err, req, res, next) => {
   res.status(500).send({ error: 'Something went wrong on the server!' });
 });
 
+// Initialize database and start server
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
