f53c182f13f50b6077b8e521eeae023405aa5daa
Harpreet Singh <happu20022@gmail.com>
2026-02-16 14:42:50 +0530

    added cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+    return res.status(204).end();
   }
   next();
 });
